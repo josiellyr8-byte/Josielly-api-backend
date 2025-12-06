3ca76def2d22c3559d4ae4103aa4b1c65d1467c6
Gustavo Gomes <gomesandregustavo@gmail.com>
2025-12-06 18:56:00 -0300

    Refactor prisma.ts to simplify code
    Remove comments and export the PrismaClient instance.

--- api-backend/src/prisma.ts
+++ api-backend/src/prisma.ts
@@ -1,11 +1,5 @@
-// api-backend/src/prisma.ts
-
 import { PrismaClient } from '@prisma/client';
 
-// Cria uma única instância do Prisma Client
-// Esta instância se conecta ao banco de dados usando a DATABASE_URL do seu arquivo .env
 const prisma = new PrismaClient();
 
-// Exporta a instância para que outros arquivos (como o index.ts e auth.routes.ts)
-// possam fazer consultas ao banco de dados.
 export default prisma;
